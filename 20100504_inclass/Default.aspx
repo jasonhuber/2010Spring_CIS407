88de1d34dd880e28fa9daeaa0d1576fd090fcc20
Jason Huber <jasonhuber@gmail.com>
2010-05-04 19:23:02 -0700

    Adding code for today
    Basic stuff like application and session and web.config

---
+++ 20100504_inclass/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Panel ID="pnlPage1" runat="server">
+        <asp:TextBox ID="txtName" runat="server" Text="Jason" />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit Me" 
+            onclick="btnSubmit_Click" />
+    </asp:Panel>
+    <asp:Panel ID="pnlResults" runat="server" Visible="false">
+        <asp:Label ID="lblResults" runat="server" />
+    </asp:Panel>
+            
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20100504_inclass/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["counted"] == null)
+        {
+            if (Application["currentUsers"] == null)
+            {
+                Application["currentUsers"] = 1;
+            }
+            else
+            {
+                Application["currentUsers"] = (int)Application["currentUsers"] + 1;
+            }
+            Session["counted"] = true;
+        }
+
+        string strVar = System.Configuration.ConfigurationSettings.AppSettings["JasonStatic"];
+        Response.Write(Application["currentUsers"]);
+
+        if (IsPostBack)
+        {
+            Response.Write("true!");
+        }
+        else 
+        {
+            Response.Write("false!");
+        }
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        lblResults.Text = txtName.Text;
+        pnlResults.Visible = true;
+        pnlPage1.Visible = false;
+    }
+}
