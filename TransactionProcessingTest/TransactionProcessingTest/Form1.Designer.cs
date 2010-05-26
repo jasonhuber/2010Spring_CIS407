1fc73d9fbcb2c665276664e9e8ac2733e2a5a70f
Jason Huber <jasonhuber@gmail.com>
2010-05-25 18:45:48 -0700

    Added files for the transaction processing

---
+++ TransactionProcessingTest/TransactionProcessingTest/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TransactionProcessingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.customeridDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.balanceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.huberBankCustomersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.devrystudentsp10DataSet = new TransactionProcessingTest.devrystudentsp10DataSet();
+            this.huber_BankCustomersTableAdapter = new TransactionProcessingTest.devrystudentsp10DataSetTableAdapters.Huber_BankCustomersTableAdapter();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtTimes = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.huberBankCustomersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devrystudentsp10DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customeridDataGridViewTextBoxColumn,
+            this.customerNameDataGridViewTextBoxColumn,
+            this.balanceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.huberBankCustomersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 209);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // customeridDataGridViewTextBoxColumn
+            // 
+            this.customeridDataGridViewTextBoxColumn.DataPropertyName = "Customerid";
+            this.customeridDataGridViewTextBoxColumn.HeaderText = "Customerid";
+            this.customeridDataGridViewTextBoxColumn.Name = "customeridDataGridViewTextBoxColumn";
+            // 
+            // customerNameDataGridViewTextBoxColumn
+            // 
+            this.customerNameDataGridViewTextBoxColumn.DataPropertyName = "CustomerName";
+            this.customerNameDataGridViewTextBoxColumn.HeaderText = "CustomerName";
+            this.customerNameDataGridViewTextBoxColumn.Name = "customerNameDataGridViewTextBoxColumn";
+            // 
+            // balanceDataGridViewTextBoxColumn
+            // 
+            this.balanceDataGridViewTextBoxColumn.DataPropertyName = "Balance";
+            this.balanceDataGridViewTextBoxColumn.HeaderText = "Balance";
+            this.balanceDataGridViewTextBoxColumn.Name = "balanceDataGridViewTextBoxColumn";
+            // 
+            // huberBankCustomersBindingSource
+            // 
+            this.huberBankCustomersBindingSource.DataMember = "Huber_BankCustomers";
+            this.huberBankCustomersBindingSource.DataSource = this.devrystudentsp10DataSet;
+            // 
+            // devrystudentsp10DataSet
+            // 
+            this.devrystudentsp10DataSet.DataSetName = "devrystudentsp10DataSet";
+            this.devrystudentsp10DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // huber_BankCustomersTableAdapter
+            // 
+            this.huber_BankCustomersTableAdapter.ClearBeforeFill = true;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(12, 25);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // txtTimes
+            // 
+            this.txtTimes.Location = new System.Drawing.Point(12, 64);
+            this.txtTimes.Name = "txtTimes";
+            this.txtTimes.Size = new System.Drawing.Size(100, 20);
+            this.txtTimes.TabIndex = 2;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(15, 129);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(97, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Try to Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How much?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "How Many Times?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Which Customer?";
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(12, 103);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerId.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 378);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtTimes);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.huberBankCustomersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devrystudentsp10DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private devrystudentsp10DataSet devrystudentsp10DataSet;
+        private System.Windows.Forms.BindingSource huberBankCustomersBindingSource;
+        private devrystudentsp10DataSetTableAdapters.Huber_BankCustomersTableAdapter huber_BankCustomersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customeridDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn balanceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtTimes;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerId;
+    }
+}
+
---
+++ TransactionProcessingTest/TransactionProcessingTest/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace TransactionProcessingTest
+{
+	public partial class Form1: Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGrid();
+
+        }
+
+        private void LoadGrid()
+        {
+            this.huber_BankCustomersTableAdapter.Fill(this.devrystudentsp10DataSet.Huber_BankCustomers);
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            System.Data.SqlClient.SqlConnection connection =
+                new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TransactionProcessingTest.Properties.Settings.devrystudentsp10ConnectionString"].ConnectionString);
+            connection.Open();
+            System.Data.SqlClient.SqlCommand command = new System.Data.SqlClient.SqlCommand();
+            command.Connection = connection;
+            System.Data.SqlClient.SqlTransaction transaction = connection.BeginTransaction(IsolationLevel.RepeatableRead);
+            command.Transaction = transaction;
+            string sql = "";
+            bool isGood = true;
+            for (int i = 0; i < int.Parse(txtTimes.Text); i++)
+            {
+                sql = "update huber_bankcustomers set balance = balance - @balance where customerid = @customerid";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@balance",int.Parse(txtAmount.Text));
+                command.Parameters.AddWithValue("@customerid",txtCustomerId.Text);
+                command.CommandText = sql;
+                command.ExecuteNonQuery();
+
+                sql = "select balance from huber_bankcustomers where customerid = @customerid";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@customerid", txtCustomerId.Text);
+                command.CommandText = sql;
+                if (int.Parse(command.ExecuteScalar().ToString()) < 0)
+                {
+                    transaction.Rollback();
+                    isGood = false;
+                    i = int.Parse(txtTimes.Text);
+                    MessageBox.Show("Sorry that transaction was rolled back. The customer is not allowed to go below a 0 balance.");
+                }
+                    
+            }
+            if (isGood)
+            {
+                transaction.Commit();
+            }
+            LoadGrid();
+            
+        }
+
+      
+	}
+}
