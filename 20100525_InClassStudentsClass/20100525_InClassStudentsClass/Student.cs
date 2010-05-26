600b61cde3ceedcb87d2a43ec2c2867be5293358
Jason Huber <jasonhuber@gmail.com>
2010-05-25 21:00:50 -0700

    Added code for the in class class passing an array

---
+++ 20100525_InClassStudentsClass/20100525_InClassStudentsClass/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20100525_InClassStudentsClass
+{
+    public class Student
+    {
+        private string _dnumber;
+
+        public string dnumber  {
+            get { return _dnumber; }
+            set {
+                    if (value.ToUpper().Substring(0, 1) != "D")
+                    {
+                        throw new Exception("You need to enter a D number (those start with D)");
+                    }
+                    else
+                    { _dnumber = value; }
+                }
+        }
+
+    }
+
+    public class Course
+    {
+        //http://www.codeproject.com/KB/cs/csharp_property_array.aspx
+        private Student[] _students = new Student[10];
+
+        public Course()
+        {
+            _students[0] = new Student();
+            _students[1] = new Student();
+            _students[2] = new Student();
+            _students[3] = new Student();
+            _students[4] = new Student();
+            _students[5] = new Student();
+            _students[6] = new Student();
+            _students[7] = new Student();
+            _students[8] = new Student();
+            _students[9] = new Student();
+
+        }
+         
+        public Student[] students{
+            get { return _students; } 
+            set{_students = value;}
+            }
+    }
+}
---
+++ 20100525_InClassStudentsClass/20100525_inclass_studentsCalss_consumer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _20100525_inclass_studentsCalss_consumer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                _20100525_InClassStudentsClass.Student[] mystudents = new _20100525_InClassStudentsClass.Student[10];
+                _20100525_InClassStudentsClass.Course myCourse = new _20100525_InClassStudentsClass.Course();
+                mystudents = myCourse.students;
+
+                mystudents[0].dnumber = "Jason was where?";
+                MessageBox.Show(mystudents[0].dnumber);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
